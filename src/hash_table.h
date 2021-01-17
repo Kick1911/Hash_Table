a001e13d14fb8d0e61ea4ee0bf8b7ab6f78d0db5
Kickness <caswallengelsman6@gmail.com>
2021-01-17 17:42:26 +0200

    Fix indentation

--- src/hash_table.h
+++ src/hash_table.h
@@ -4,26 +4,26 @@
 #include <stdint.h>
 
 typedef struct h_node{
-	char* key;
-	void* value;
+    char* key;
+    void* value;
 }h_node_t;
 
 typedef struct {
-	uint32_t capacity;
-	uint32_t increm;
-	uint32_t size;
-	void** hash_table; /* dl_link_t** */
-	void* elements; /* dl_link_t* */
-	void (*free)(void*);
+    uint32_t capacity;
+    uint32_t increm;
+    uint32_t size;
+    void** hash_table; /* dl_link_t** */
+    void* elements; /* dl_link_t* */
+    void (*free)(void*);
 }h_table_t;
 
 typedef struct{
-	void* node; /* dl_node_t */
+    void* node; /* dl_node_t */
 } h_iter_t;
 
 #define H_SIZE(ht) ((h_table_t*)(ht))->size
 
-h_table_t* h_create_table(void (*free_cb)(void*));
+h_table_t* h_create_table(h_table_t*);
 void h_free_table(h_table_t*);
 h_iter_t* h_iter(h_table_t*);
 int h_next(h_iter_t*, char**, void**);
--- src/utils/hash.h
+++ src/utils/hash.h
@@ -11,38 +11,38 @@
 #define HN_KEY(dl_n) ((h_node_t*)DN_DATA(dl_n))->key
 
 static uint64_t mod(int64_t a, int64_t b){ 
-	int64_t d = a/b;
-	int64_t r = a - b * d;
-	if( d < 1 ){
-		a = b + r;
-		return a - b * (a / b);
-	}
-	return r;
+    int64_t d = a/b;
+    int64_t r = a - b * d;
+    if( d < 1 ){
+        a = b + r;
+        return a - b * (a / b);
+    }
+    return r;
 }
 
 static int64_t hash(const char* k, uint64_t limit){
-	int64_t h = 0;
-	size_t len = strlen(k);
+    int64_t h = 0;
+    size_t len = strlen(k);
 
     if(!len) return -1;
-	while(--len)
-		h += k[len] - k[len+1];
-	return mod(h, limit);
+    while(--len)
+        h += k[len] - k[len+1];
+    return mod(h, limit);
 }
 
 static int64_t get_hash(h_table_t* ht, const char* k){
-	int64_t hk = hash(k, ht->increm);
+    int64_t hk = hash(k, ht->increm);
 
     if(hk < 0) return -1;
-	while( ht->hash_table[hk] && strcmp(HN_KEY(ht->hash_table[hk]), k) )
-		hk += ht->increm;
-	return hk;
+    while( ht->hash_table[hk] && strcmp(HN_KEY(ht->hash_table[hk]), k) )
+        hk += ht->increm;
+    return hk;
 }
 
 static h_node_t* get_entry(h_table_t* ht, const char* k){
-	int64_t hk = get_hash(ht, k);
-	if(hk < 0 || hk > ht->capacity) return NULL;
-	return (ht->hash_table[hk])? DN_DATA(ht->hash_table[hk]) : NULL;
+    int64_t hk = get_hash(ht, k);
+    if(hk < 0 || hk > ht->capacity) return NULL;
+    return (ht->hash_table[hk])? DN_DATA(ht->hash_table[hk]) : NULL;
 }
 
 #endif
